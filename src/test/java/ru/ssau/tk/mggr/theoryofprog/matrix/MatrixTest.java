f2998c75de360063377f5f2f01bc4739fdb08ef4
Megrelishvili <bagrat37@mail.ru>
2020-10-25 14:52:30 +0400

    test package

---
+++ src/test/java/ru/ssau/tk/mggr/theoryofprog/matrix/MatrixTest.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.mggr.theoryofprog.matrix;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MatrixTest {
+
+    @Test
+    public void testGetRows() {
+    }
+
+    @Test
+    public void testTestGetRows() {
+    }
+
+    @Test
+    public void testGetColumns() {
+    }
+
+    @Test
+    public void testGetAt() {
+    }
+
+    @Test
+    public void testSetAt() {
+    }
+}
